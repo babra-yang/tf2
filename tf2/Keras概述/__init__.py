585cf23938172d59949b7d641f532a51b472d6dc
vivi <353732132@qq.com>
2022-01-11 23:10:29 +0800

    first commit

---
+++ tf2/Keras概述/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/9 11:11 下午
+# @Author  : quanziwei
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
+if __name__ == "__main__":
+    print('good luck')
---
+++ tf2/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2022/1/11 11:02 下午
+# @Author  : quanziwei
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
+if __name__ == "__main__":
+    print('good luck')
